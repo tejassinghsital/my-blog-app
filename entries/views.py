2766c60a04dbb895ba1e065c7f02255f3bd099c2
Tejjassiingh <tejassingh1992001@gmail.com>
2020-02-18 21:02:32 -0800

    my first blog application,first commit

---
+++ entries/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Entry
+
+class HomeView(ListView):
+	model = Entry
+	template_name = 'entries/index.html'
+	context_object_name = "blog_entries"
+	ordering = ['-entry_date']
+	paginate_by = 3
+
+class EntryView(DetailView):
+	model = Entry
+	template_name = 'entries/entry_detail.html'	
+
+# Create your views here.
